4fe2511923720dad1cba96d6ee29c23466a0326b
sohidul Islam Ananto <anontom90@gmail.com>
2024-05-22 18:53:17 +0600

    arraylistadded

--- ArrayList/01.ArrayList.ts
+++
@@ -1,6 +0,0 @@
-// function funTion() : void{
-//     let naration: string = "My Name is Ananto";
-//     return console.log(naration);
-// }
-
-// funTion();
---
+++ ArrayList/arrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+
+        ArrayList<String> myArrayList = new ArrayList<>();
+
+        myArrayList.add("all");
+        myArrayList.add("Okk");
+
+        System.out.println(myArrayList);
+    }
+}
